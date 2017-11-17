aaa4c29913dc5b6a360a39764504bc00a2a125cf
davidfoster0127 <davidf94@uw.edu>
2017-11-17 11:44:24 -0800

    added a few os and the start of ourthread

---
+++ os.h
@@ -0,0 +1,26 @@
+#ifndef OS_H
+#define OS_H
+
+#include "pqueue.h"
+#include "pthread.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define IO_PROCESS_LIMIT 50
+#define CI_PROCESS_LIMIT 25
+#define PC_PAIR_PROCESS_LIMIT 10
+#define MRU_PROCESS_LIMIT 10
+
+typedef struct timer {
+    unsigned int counter;
+
+} timer_s;
+
+int signalTimer();
+int generatePCBs();
+void timer_pseudoISR();
+void IO_pseudoISR();
+
+
