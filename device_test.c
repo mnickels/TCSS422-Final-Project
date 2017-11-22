8c7967064b22666cdeb71777fdc5babf79239f27
Slavik <ysalo@uw.edu>
2017-11-22 14:10:37 -0800

    add array traps to the pcb

--- device_test.c
+++ device_test.c
@@ -1,4 +1,5 @@
 #include "iodevice.h"
+#include "pcb.h"
 
 int main() {
 	DEVICE_p device1 = device_constructor(1);
@@ -6,6 +7,9 @@ int main() {
 	printf("TIMER: %u\n", device1->timer);
 	//space for testing the queue
 
+
+	PCB_p p1 = pcb_constructor();
+	
 	device_set_timer(device1, 100);
 	printf("AFTER SET\nTIMER: %u\n", device1->timer);
 	
@@ -13,7 +17,7 @@ int main() {
 	DEVICE_p device2 = device_constructor(2);
 	printf("AFTER CONSTRUCTION\nID: %u\n", device2->io_id);
 	printf("TIMER: %u\n", device2->timer);
-	//space for testing the queue
+
 
 	device_set_timer(device2, 200);
 	printf("AFTER SET\nTIMER: %u\n", device2->timer);
