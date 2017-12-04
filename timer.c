d17c45539b6f0bddd4baff1317c551031161f698
Mike Nickels <mc.nickels2@gmail.com>
2017-12-04 00:20:42 +0000

    Finished timer (not tested)

---
+++ timer.c
@@ -0,0 +1,53 @@
+#include <time.h>
+#include <assert.h>
+#include "timer.h"
+
+TIMER_p timer_constructor() {
+	TIMER_p timer = calloc(1, sizeof(timer_s));
+	if (timer) {
+		timer->counter = -1;
+		int rc = pthread_mutex_init(&(timer->mutex), NULL);
+		assert(rc == 0);
+		pthread_create(&(timer->timer_thread), NULL, &timer_run(), timer);
+	}
+	return timer;
+}
+
+void timer_deconstructor(* TIMER_p timer) {
+	if (timer) {
+		int rc = pthread_cancel(&((* timer)->timer_thread));
+		assert(rc == 0);
+
+		free(* timer);
+		timer = NULL;
+	}
+}
+
+void * timer_run(TIMER_p timer) {
+	struct timespec * ts;
+	ts->tv_sec = 0;
+	ts->tv_nsec = QUANTUM_SCALAR;
+	for(;;) {
+		timer_tick(timer);
+		nanosleep(ts, NULL);
+	}
+}
+
+void timer_tick(TIMER_p timer) {
+	pthread_mutex_lock(&(timer->mutex));
+	if (timer->counter == 0) {
+		timer->counter--;
+		// timer interrupt
+	} else if (timer->counter == -1) {
+		// timer is disabled; do nothing
+	} else {
+		timer->counter--;
+	}
+	pthread_mutex_unlock(&(timer->mutex));
+}
+
+void set_timer(TIMER_p timer, unsigned int quantum) {
+	pthread_mutex_lock(&(timer->mutex));
+	timer->counter = quantum;
+	pthread_mutex_unlock(&(timer->mutex));
+}
